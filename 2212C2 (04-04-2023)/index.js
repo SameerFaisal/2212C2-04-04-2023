87424b218253217f1a92d698ee4738bb8d5ebe6d
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-04-04 12:58:34 +0500

    2212C2 04-04-2023

---
+++ 2212C2 (04-04-2023)/index.js
@@ -0,0 +1,82 @@
+// function printArray(a) 
+// {
+//      var len = a.length //4
+//      i = 0;
+//      if (len == 0)
+//         console.log("Empty Array");
+//      else 
+//      {
+//          do 
+//          {
+//              document.write(a[i]+"<br>");
+//          } while (++i < len);
+//      }
+// }
+
+// printArray([5,67,89,10])
+// var a = 1;  
+// var b = 0;  
+// while (a <= 3)  
+// {  
+//    a++;  
+//    b += a * 2;  
+//    document.write(b);
+// }
+// let salary=10000
+
+// function increment(sal){
+//    sal=sal+5000
+//     alert("Your Ramzan Salary is: "+sal)
+// }
+// increment(salary)
+// alert("Your normal salary is: "+salary)
+
+// let howMany=+prompt("Enter how many values you want to enter")
+// let num=[1,2,3,4]
+// // function userInput(){
+// // for(let i=0;i<howMany;i++){
+// //     num[i]=+prompt("Enter the "+i+"th value")
+// // }
+
+// // }
+
+// function printArray(num){
+// for(let i=0;i<num.length;i++){
+//     document.write(num[i]+"<br>")
+// }
+// }
+// // userInput()
+// printArray(num)
+
+
+// var std1=["Shaheer","Karachi","20"]
+
+// let std1={
+//     name:"Shaheer",
+//     city:"Karachi",
+//     age:20,
+//     contact:["03001111111","03002222222"]
+// } 
+// document.write(std1.name+"<br>"+std1.age+"<br>"+std1.city)
+
+// document.write(std1.contact[1])
+
+// document.write(std1)
+
+let std1={
+    name:"Shaheer",
+    city:"Karachi",
+    age:20,
+    contact:{
+        phone1:"03001111111",
+        phone2:"03002222222"
+    }
+} 
+
+std1.country="Pakistan"
+
+console.log(std1)
+
+// document.write(std1.contact.phone2)
+
+
